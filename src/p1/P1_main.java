f15f747468e5499b95f9d32b42b45b1adb194508
fzapp <fzapp@smail.uni-loeln.de>
2025-12-17 13:02:50 +0100

    aenderung_Einfuehrung_Aufgabenblatt9_Aufg1fix

--- src/p1/P1_main.java
+++ src/p1/P1_main.java
@@ -1,10 +1,16 @@
 package p1;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.stream.*;
 
 public class P1_main {
+	
 
 	public static void main(String[] args) {
+		System.out.println(Arrays.toString(randomArray(4)));
+		System.out.println(Arrays.toString(randomArray2(4)));
+		System.out.println(Arrays.toString(randomArray3()));
+		System.out.println(Arrays.toString(randomArray4(4)));
 		// TODO Auto-generated method stub
 
 	}
@@ -22,6 +28,7 @@ public class P1_main {
 		int[] randArray = new int[x];
 		Random generator = new Random();
 		IntStream randIntsStream = generator.ints(0, x);
+		randIntsStream = randIntsStream.limit(x);
 		int[] randInts = randIntsStream.toArray();
 		for(int i=0; i<randArray.length; i++) {
 			randArray[i] = randInts[i];
@@ -33,6 +40,7 @@ public class P1_main {
 		int[] randArray = new int[6];
 		Random generator = new Random();
 		IntStream randIntsStream = generator.ints(55, 76);
+		randIntsStream = randIntsStream.limit(6);
 		int[] randInts = randIntsStream.toArray();
 		for(int i=0; i<randArray.length; i++) {
 			randArray[i] = randInts[i];
@@ -44,6 +52,7 @@ public class P1_main {
 		int[] randArray = new int[n+1];
 		Random generator = new Random();
 		IntStream randIntsStream = generator.ints(1, 51);
+		randIntsStream = randIntsStream.limit(n+1);
 		int[] randInts = randIntsStream.toArray();
 		for(int i=0; i<randArray.length -1; i++) {
 			randArray[i] = randInts[i];
---
+++ src/p2/P2_main.java
@@ -0,0 +1,12 @@
+package p2;
+
+public class P2_main {
+	int[][] grid;
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
